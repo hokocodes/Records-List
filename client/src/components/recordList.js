604e11dff22dbb0e4ed744e6abcd9a4cc53070af
hokole <hokole551@gmail.com>
2023-10-27 10:56:03 -0500

    Fixed create person bug

--- client/src/components/recordList.js
+++ client/src/components/recordList.js
@@ -21,14 +21,14 @@ export default function RecordList() {
  const [records, setRecords] = useState([]);
   // This method fetches the records from the database.
  useEffect(() => {
-   async function getRecords() {
-     const response = await fetch(`http://localhost:5000/record/`);
+   function getRecords() {
+     const response = fetch(`http://localhost:5000/record/`);
       if (!response.ok) {
        const message = `An error occurred: ${response.statusText}`;
        window.alert(message);
        return;
      }
-      const records = await response.json();
+      const records = response.json();
      setRecords(records);
    }
     getRecords();
